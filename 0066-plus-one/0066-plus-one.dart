d26d54a2458bccdfb0cd158ba7a9ecd4602679ca
Sweta Maharjan <60056050+itssss-sweta@users.noreply.github.com>
2024-03-13 22:15:57 +0545

    Time: 307 ms (91.74%), Space: 151.7 MB (22.73%) - LeetHub

--- 0066-plus-one/0066-plus-one.dart
+++ 0066-plus-one/0066-plus-one.dart
@@ -1,25 +1,19 @@
 class Solution {
   List<int> plusOne(List<int> array) {
-   for (int i = array.length - 1; i >= 0; i--) {
+  for (int i = array.length - 1; i >= 0; i--) {
     int sum = array[i] + 1;
     print(sum);
-    String sumString = sum.toString();
-    if (sumString.length == 1) {
+    if (sum < 10) {
       array[i] = sum;
       break;
     }
-    if (sumString.length == 2) {
-      array[i] = int.parse(
-          sumString.substring(sumString.length - 1, sumString.length));
+    if (sum >= 10) {
+      array[i] = sum % 10;
     }
     if (i == 0 && sum >= 10) {
-      array.insert(
-          0,
-          int.parse(
-              sumString.substring(sumString.length - 2, sumString.length - 1)));
+      array.insert(0, 1);
     }
   }
-  print(array);
   return array;
 }
 }
