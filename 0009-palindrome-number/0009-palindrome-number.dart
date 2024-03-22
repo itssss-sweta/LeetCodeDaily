1c2592996b6fa59bac705f3f65e360400b217e91
Sweta Maharjan <60056050+itssss-sweta@users.noreply.github.com>
2024-03-22 22:03:03 +0545

    Time: 532 ms (91.29%), Space: 152.2 MB (40.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.dart
@@ -0,0 +1,19 @@
+class Solution {
+
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false; // Negative numbers are not palindromes
+        }
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = (x / 10).toInt();
+        }
+
+        return original == reversed;
+    }
+}
