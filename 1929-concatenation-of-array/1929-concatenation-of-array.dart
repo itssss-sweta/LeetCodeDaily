5464adf626a2ced6197050354661087b12818ac2
Sweta Maharjan <60056050+itssss-sweta@users.noreply.github.com>
2024-03-22 21:39:02 +0545

    Time: 382 ms (17.13%), Space: 151.9 MB (23.76%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.dart
@@ -0,0 +1,18 @@
+class Solution {
+  List<int> getConcatenation(List<int> nums) {
+      List<int> concatenateNumber = [];
+  List<String> concatenateNumberIndex = [];
+  concatenateNumber.addAll(nums);
+  String index;
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < concatenateNumber.length; i++) {
+      index = 'num[$i]';
+      concatenateNumberIndex.add(index);
+    }
+  }
+  print(concatenateNumberIndex);
+  concatenateNumber.addAll(nums);
+  print(concatenateNumber);
+      return concatenateNumber;
+  }
+}
